7ae6b47f2b8d3c79c39ded59012e0d49c36696d1
sandeep <sandeepdtu360@gmail.com>
2024-06-19 17:07:58 +0530

    modified object.js file

--- oops-js/object.js
+++ oops-js/object.js
@@ -11,3 +11,42 @@ The constructor is called: The constructor function is called with the specified
 The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
 
 */
+// Objects can be created using object literals, the Object constructor, or by using classes
+
+// Object Literals:
+let person = {
+    name:'sandeep',
+    age: 25,
+    greet:function(){
+        console.log(`hello ${this.name}`);
+    }
+}
+console.log(person);
+console.log(person.age);
+
+//Using the Object Constructor:
+
+let User= new Object();
+User.name='sonam';
+User.age=23;
+User.greet = function(){
+    console.log(`hello ${this.name}`);
+}
+console.log(User);
+console.log(User.age);
+
+
+// Using Classes (ES6):
+
+class Person{
+    constructor(name, id){
+        this.name=name;
+        this.id=id;
+    }
+    greet(){
+        console.log(`hello ${this.name}`);
+    }
+    }
+let per = new Person('sanhya', '123');
+console.log(per.greet());
+console.log(per.id);
---
+++ oops-js/prototype.js
@@ -0,0 +1,10 @@
+const myObject = {
+    city: "Madrid",
+    greet() {
+      console.log(`Greetings from ${this.city}`);
+    },
+  };
+  
+  myObject.greet(); // Greetings from Madrid
+  Object.getPrototypeOf(myObject);
+  
